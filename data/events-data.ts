a53ab6319f54e09ca6244ef0e8a7999f55890dc1
yosefbeder <dryosefbeder@gmail.com>
2021-08-29 17:53:01 +0200

    Serve images statically

--- data/events-data.ts
+++ data/events-data.ts
@@ -6,8 +6,7 @@ const DUMMY_EVENTS = [
       'Everyone can learn to code! Yes, everyone! In this live event, we are going to go through all the key basics and get you started with programming as well.',
     location: 'Somestreet 25, 12345 San Somewhereo',
     date: '2021-05-12',
-    image:
-      'https://raw.githubusercontent.com/mschwarzmueller/nextjs-course-code/03-prj-routing/public/images/coding-event.jpg',
+    image: '/images/coding-event.jpg',
     isFeatured: false,
   },
   {
@@ -17,8 +16,7 @@ const DUMMY_EVENTS = [
       "We know: Networking is no fun if you are an introvert person. That's why we came up with this event - it'll be so much easier. Promised!",
     location: 'New Wall Street 5, 98765 New Work',
     date: '2021-05-30',
-    image:
-      'https://raw.githubusercontent.com/mschwarzmueller/nextjs-course-code/03-prj-routing/public/images/extrovert-event.jpg',
+    image: '/images/extrovert-event.jpg',
     isFeatured: true,
   },
   {
@@ -28,8 +26,7 @@ const DUMMY_EVENTS = [
       'You probably need no help with networking in general. But focusing your energy correctly - that is something where most people can improve.',
     location: 'My Street 12, 10115 Broke City',
     date: '2022-04-10',
-    image:
-      'https://raw.githubusercontent.com/mschwarzmueller/nextjs-course-code/03-prj-routing/public/images/introvert-event.jpg',
+    image: '/images/introvert-event.jpg',
     isFeatured: true,
   },
 ];
